4a9a49665228a2d2562ab097e711ceb5ca3b0119
joslcan <josluicanvas@gmail.com>
2025-03-14 01:52:28 -0500

    Agrega amigos secretos

--- app.js
+++ app.js
@@ -1,8 +1,52 @@
-let listaAmigosSecretos = [];
+let listaAmigosSecretos = ['Amigos Secretos por sortear:'];
+
 
 //Obtener el nombre del amigo en el box
 function obtenerAmigo() {
     return String(document.getElementById('amigo').value);
+}
+
+//Guardar al amigo en la lista
+function agregarAmigo(){
+    
+    if (listaAmigosSecretos.length == 1) {
+        inciarListaAmigosSecretos();    
+    }
+    
+    let amigo = obtenerAmigo();
+    listaAmigosSecretos.push(amigo);
+    actualizarLista(listaAmigosSecretos,'listaAmigos');
+    limpiarBox();
+    return;
+
+    /*for (let i = 0; i < listaAmigosSecretos.length; i++) {
+        let textoAmigo = listaAmigosSecretos[i];
+        asignarTextoAlElemento('ul',textoAmigo);
+    }
+    */
+}
+
+//Limpia la lista que se indique
+function limpiarLista(listaDestino) {
+    document.getElementById(listaDestino).removeChild(li);
+    return;
+}
+
+//Iniciar la lista con el texto
+function inciarListaAmigosSecretos() {
+    const li = document.createElement('li');
+    li.textContent = listaAmigosSecretos;
+    document.getElementById('listaAmigos').appendChild(li);
+    return;
+}
+
+//Aumentar el tamaño de la lista de amigos secretos
+function actualizarLista(arreglo,listaDestino) {
+    const li = document.createElement('li');
+    li.textContent = arreglo[arreglo.length-1];
+    li.setAttribute('type','square');
+    document.getElementById(listaDestino).appendChild(li);
+    return;
 }
 
 
@@ -10,17 +54,20 @@ function obtenerAmigo() {
 //la funcion que desactiva un boton
 function desactivarBoton(boton) {
     document.getElementById(boton).setAttribute('disabled','true');
+    return;
 }
 
 //La funcion que activa un boton
 function activarBoton(boton) {
     document.getElementById(boton).removeAttribute('disabled');
+    return;
 }
 
 //La funcion que asigna un texto
 function asignarTextoAlElemento(elemento,texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
+    return;
 }
 
 //La funcion que limpia una caja de texto
@@ -30,12 +77,7 @@ function limpiarBox(){
 }
 
 
-//La funcion que agregará un amigo
-function agregarAmigo() {
-    
-}
-
 //La funcion que sorteará un amigo
 function sortearAmigo() {
-    
+    return;
 }
