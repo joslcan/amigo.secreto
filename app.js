6405bce4832482fa8b6208cb154f8bc0f65e3eb6
joslcan <josluicanvas@gmail.com>
2025-03-13 19:23:00 -0500

    Aumenta funciones de boton
    Agrega las bases y las funciones para disabled botones

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigosSecretos = [];
+
+function desactivarBoton(boton) {
+    document.getElementById(boton).setAttribute('disabled','true');
+}
+
+function activarBoton(boton) {
+    document.getElementById(boton).removeAttribute('disabled');
+}
+
+desactivarBoton('añadir');
+activarBoton('añadir');
+
+
+function asignarTextoAlElemento(elemento,texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
+    
+function limpiarBox(){
+    document.getElementById('amigo').value = '';
+    return;
+}
+    
+    
+    
+function agregarAmigo() {
+    
+}
+    
+function sortearAmigo() {
+    
+}
