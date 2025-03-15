f857a05017c31af5519c5bf5da81748c6334c1a3
joslcan <josluicanvas@gmail.com>
2025-03-15 00:18:46 -0500

    Ordena las funciones y limpia los comentarios

--- app.js
+++ app.js
@@ -1,51 +1,117 @@
+/* ===Inicializacion de la lista de Amigos Secretos=== */
+
 let listaAmigosSecretos = ['Amigos Secretos por sortear:'];
 
-//Obtener el nombre del amigo en el box
-function obtenerAmigo() {
-    return String(document.getElementById('amigo').value);
-}
+
+
+/* ===Funciones principales=== */
 
 //Guardar al amigo en la lista
 function agregarAmigo(){
-    
+    //Condicional por si el usuario activa el boton y quiere agregar el campo vacio
     if (document.getElementById('amigo').value == ''){
         alert('Agrega un nombre valido por favor\n( No dejes el campo vacio \^\_\~ )');
+
     } else {
-        //Obtener el nombre que el usuario puso en el box
+        //Obtener el nombre del amigo
         let amigo = obtenerAmigo();
+
         //Agregarlo al arreglo 
         listaAmigosSecretos.push(amigo);
         
         //Actualiza la Lista
         actualizaLista (listaAmigosSecretos,'listaAmigos');    
         
-        //Limpia la caja para ingresar otro nombre
+        //Limpia la caja para ingresar otro amigo
         limpiarBox();
-        
-        //Activa el boton para sortear
-        activarBoton('sortear');
         
         //Bloquear el boton añadir
         desactivarBoton('añadir');
+        
+        //Activa el boton para sortear
+        activarBoton('sortear');
+             
     }
 
     return;
 }
 
+//La funcion que sorteará un amigo
+function sortearAmigo() {
+    //Se genera un numero random
+    let amigoSeleccionado = calcularAleatorioSinInicial(listaAmigosSecretos);
+    
+    //Se usa el amigo seleccionado para mostrar en 'resultado'
+    asignarTextoAlElemento('resultado',`Felicidades!! Tu amigo secreto es: ${listaAmigosSecretos[amigoSeleccionado]}`);
+    
+    //Se elimina al amigo de la lista
+    eliminarDeLista(listaAmigosSecretos,amigoSeleccionado);
+
+    //Se actualiza la lista de amigos secretos que queda
+    actualizaLista (listaAmigosSecretos,'listaAmigos');
+        
+    //En caso que no hubieran mas amigos por sortear
+    if (listaAmigosSecretos.length == 1) {
+        limpiarLista('listaAmigos');
+        asignarTextoAlElemento('listaAmigos','Ya no hay mas amigos secretos por sortear');
+        desactivarBoton('sortear');
+    }
+    
+    return;
+}
+
+
+
+/* ===Funcion para generar un numero pseudo-aleatorio=== */
+
+//Genera un numero pseudo-random entre el 1 y el largo del arreglo
+function calcularAleatorioSinInicial(listaDestino) {
+    return Math.floor(Math.random()*(listaDestino.length-2))+1;
+}
+
+
+
+/* ===Funciones para cuadros de texto=== */
+
+//Obtener el nombre del amigo en el box
+function obtenerAmigo() {
+    return String(document.getElementById('amigo').value);
+}
+
+//La funcion que limpia una caja de texto
+function limpiarBox() {
+    document.getElementById('amigo').value = '';
+    return;
+}
+
+//La funcion que asigna un texto aun elemento
+function asignarTextoAlElemento(elemento,texto) {
+    let elementoHTML = document.getElementById(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+
+
+/* ===Funciones para arreglos=== */
+
+//Elimina datos de un arreglo
+function eliminarDeLista(arreglo,indice) {
+    arreglo.splice(indice,1);
+    return;
+}
+
+
+
+/* ===Funciones para las listas=== */
+
 //Actualiza la lista de amigos secretos a elegir
-function actualizaLista(arreglo,listaDestino){
+function actualizaLista(arreglo,listaDestino) {
     limpiarLista(listaDestino);
     inciarListaAmigosSecretos();
     refrescaLista (arreglo,listaDestino);
     return;
 }
-
-//Elimina datos de un arreglo
-function eliminarDeLista(arreglo,indice) {
-    arreglo.splice(indice,1)
-    return;
-}
-
 
 //Limpia la lista que se indique
 function limpiarLista(listaDestino) {
@@ -64,8 +130,7 @@ function inciarListaAmigosSecretos() {
     return;
 }
 
-//Se vuelve a generar la lista con los nombres del arreglo
-//Se usaria un bucle for
+//Generar la lista con los nombres del arreglo
 function refrescaLista (arreglo,listaDestino) {
     for (let l=1 ; l < arreglo.length ; l++) {
         const li = document.createElement('li');
@@ -76,6 +141,10 @@ function refrescaLista (arreglo,listaDestino) {
     return;
 }
 
+
+
+/* ===Funciones para los botones=== */
+
 //la funcion que desactiva un boton
 function desactivarBoton(boton) {
     document.getElementById(boton).disabled = true;
@@ -85,57 +154,5 @@ function desactivarBoton(boton) {
 //La funcion que activa un boton
 function activarBoton(boton) {
     document.getElementById(boton).disabled = false;
-    return;
-}
-
-//La funcion que asigna un texto
-function asignarTextoAlElemento(elemento,texto){
-    let elementoHTML = document.getElementById(elemento);
-    elementoHTML.innerHTML = texto;
-    return;
-}
-
-//La funcion que limpia una caja de texto
-function limpiarBox(){
-    document.getElementById('amigo').value = '';
-    return;
-}
-
-//Genera un numero pseudo-random entre el 1 y el largo del arreglo
-function calcularAleatorioSinInicial(listaDestino){
-    return Math.floor(Math.random()*(listaDestino.length-2))+1;
-}
-
-//La funcion que sorteará un amigo
-function sortearAmigo() {
-    //Presiona el boton
-    //Se genera un numero random
-    //(el numero tiene que ser entre 1 y el largo del arreglo)
-    //Se usa ese numero para seleccionar un nombre del arreglo
-
-    let amigoSeleccionado = calcularAleatorioSinInicial(listaAmigosSecretos);
-    
-    //Se usa el amigo seleccionado para mostrar en 'resultado' con un texto adicional
-    //El texto adicional "Su amigo secreto es:"
-    asignarTextoAlElemento('resultado',`Felicidades!! \nTu amigo secreto es:\n\t${listaAmigosSecretos[amigoSeleccionado]}`)
-    
-    //Se usa ese numero para eliminar ese nombre del arreglo
-    //Se elimina la lista
-    eliminarDeLista(listaAmigosSecretos,amigoSeleccionado);
-
-    //Se actualiza la lista de amigos secretos que queda
-    //Se elimina todos los elementos de la lista
-    //NO SE ELIMINA LOS ELEMENTOS DEL ARREGLO!!
-    //Se vuelve a iniciar la lista y se refresca el listado
-    actualizaLista (listaAmigosSecretos,'listaAmigos');
-    
-    
-    //
-    if (listaAmigosSecretos.length == 1) {
-        limpiarLista('listaAmigos');
-        asignarTextoAlElemento('listaAmigos','Ya no hay mas amigos secretos por sortear');
-        desactivarBoton('sortear');
-    }
-    
     return;
 }
